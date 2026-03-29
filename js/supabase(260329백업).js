c87f8c95c4ad6903eaf7f47f123e1ab8affcec8c
lim6922 <65052340+lim6922@users.noreply.github.com>
2026-03-29 19:48:13 +0900

    260329

---
+++ js/supabase(260329백업).js
@@ -0,0 +1,11 @@
+const SUPABASE_URL = "https://xbyaoqswxqpketnyypar.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhieWFvcXN3eHFwa2V0bnl5cGFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4OTA2OTQsImV4cCI6MjA4ODQ2NjY5NH0.9hPbqLS9C9c5onmb6idJUVYVidXSdkLq2ywhejDf9JI";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+    storage: window.sessionStorage
+  }
+});
